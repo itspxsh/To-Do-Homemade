1233c56b01965e0c088201918da4d074b8810830
itspxsh <poshpawarisa@gmail.com>
2023-11-03 01:12:40 +0700

    Create main.js

---
+++ main tempo/main.js
@@ -0,0 +1,29 @@
+import {clickButton,clickButton1,clickButton2,clickButton4,
+    // clickButton5,
+    changeTextColor,
+    midText,
+    endText,
+    startText,
+    sizeText,
+    toggleDisplay,
+    ontaskclick,
+closetaskclick,
+createtaskfromdiv,dragElement} from './script1.js'
+
+
+
+clickButton()
+dragElement()
+clickButton1()
+clickButton2()
+clickButton4()
+// clickButton5()
+changeTextColor()
+midText()
+endText()
+startText()
+sizeText()
+toggleDisplay()
+ontaskclick()
+closetaskclick()
+createtaskfromdiv()
